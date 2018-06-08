fefd2c2dc16b7776a1a8e694d608c31c43d1ac5b
Dominic <meiserdo@web.de>
2018-06-08 11:14:54 +0200

    serialize articles using jackson

--- src/eu/newsapp/backend/main.kt
+++ src/eu/newsapp/backend/main.kt
@@ -1,5 +1,6 @@
 package eu.newsapp.backend
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import spark.Spark.*
 
 fun main(args : Array<String>)
@@ -7,6 +8,10 @@ fun main(args : Array<String>)
 	port(8706)
 	
 	get("/api/articles.json") { req, res ->
-		"""{"dummy":true}"""
+		val articles = getArticles()
+		res.header("Content-Type", "application/json")
+		val out = res.raw().outputStream
+		jacksonObjectMapper().writeValue(out, articles)
+		out.close()
 	}
 }
